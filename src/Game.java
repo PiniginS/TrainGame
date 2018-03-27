68b6fc67e20559d541093287271f10a1bd171b55
Sergey Pinigin <pinigins55@gmail.com>
2018-03-27 11:45:14 +0600

    Add solution

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,29 @@
 public class Game {
     public static void main(String[] args) {
         Train train = new Train(100, true); //первый параметр задает максимальное количество вагонов, второй включает или выключает текстовое описание игры
-        /*
-        train.turnLight();//переключает свет вкл-выкл
-        train.checkLight();//проверяет горит ли свет, отвечает true или false
-        train.moveRight();//перейти в правый вагон
-        train.moveLeft();//перейти в левый вагон
-        train.checkAnswer(3);//проверить ответ, если ответ неверный, то поезд пересоздастся с новым количеством вагонов
-        */
+
+        int derection = -1;
+        int answer = 1;
+        while (true) {
+            if (!train.checkLight())
+                train.turnLight();
+            if (derection == -1)
+                train.moveLeft();
+            else train.moveRight();
+            if (!train.checkLight())
+                train.turnLight();
+
+            derection = derection * -1;
+            for (int i = 0; i < answer; i++) {
+                if (derection == -1) train.moveLeft();
+                else train.moveRight();
+                if (!(i==answer-1)) if (train.checkLight()) train.turnLight();
+            }
+            if (!train.checkLight()) {
+                train.checkAnswer(answer-1);
+                return;
+            }
+            answer++;
+        }
     }
 }
